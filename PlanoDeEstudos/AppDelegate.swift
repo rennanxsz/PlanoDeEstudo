fcd591a842d1bf2631a4e92b03f01fcef88ae61d
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-30 22:24:56 -0300

    Notification configuration
    Configurações adicionais da notificação.

--- PlanoDeEstudos/AppDelegate.swift
+++ PlanoDeEstudos/AppDelegate.swift
@@ -13,10 +13,32 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var center = UNUserNotificationCenter.current()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window?.tintColor = UIColor(named: "main")
+        
+        center.delegate = self
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .notDetermined {
+                let options: UNAuthorizationOptions = [.alert, .sound, .badge, .carPlay]
+                self.center.requestAuthorization(options: options, completionHandler: { (sucess, error) in
+                    
+                    if error == nil {
+                        print(sucess)
+                    }else {
+                        print(error!.localizedDescription)
+                    }
+                })
+            } else if settings.authorizationStatus == .denied {
+                print("Usuário negou a notificação")
+            }
+        }
+        
+        
+        
         return true
     }
 
@@ -42,4 +64,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
 }
